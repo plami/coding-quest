6b26db8a9bfe41a40748df757812340151080e95
plamena.y.nikolova@gmail.com <plamena.y.nikolova@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-11-21 14:09:01 +0000

    The Beginning of the project

---
+++ CodingQuest/CodingQuest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodingQuest
+//
+//  Created by User-06 on 11/21/14.
+//  Copyright (c) 2014 User-06. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodingQuest/CodingQuest/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  CodingQuest
+//
+
+//  Copyright (c) 2014 User-06. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
