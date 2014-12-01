5fdad1ece94449860f9cf13fcc9e4f056d08c03f
emiliev1994@gmail.com <emiliev1994@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-12-01 11:14:50 +0000

    Player moving with mvc model

---
+++ trunk/CodingQuest/CodingQuest/SpriteTextures.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface SpriteTextures : NSObject
+
+@property NSArray* playerRunRightTextures;
+
+-(void) createAnimationTextures;
+
+@end
+
+
