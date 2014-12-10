cfd8e8d6768f435765ce75165299a29038911224
plamena.y.nikolova@gmail.com <plamena.y.nikolova@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-12-10 11:18:14 +0000

    player returned after conflicts

---
+++ trunk/CodingQuest/Background.h
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface Background : SKSpriteNode
+
+- (instancetype)initWithBackground:(NSString *)background
+                    size:(CGSize)size
+                   speed:(CGFloat)speed;
+
+- (void)update:(NSTimeInterval)currentTime;
+
+@end
