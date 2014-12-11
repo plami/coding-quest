3912e2a39cecdffc64496ea53e67fd4504942eef
emiliev1994@gmail.com <emiliev1994@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-12-11 13:06:06 +0000

    mislq che go opsravihme nai setne

--- CodingQuest/Player.h
+++ CodingQuest/Player.h
@@ -1,12 +1,34 @@
 #import <Foundation/Foundation.h>
 #import <SpriteKit/SpriteKit.h>
 #import "GameScene.h"
+#import "GameSceneLevel2.h"
+
 @interface Player : SKSpriteNode
 
--(instancetype)initWithHeroImage: (NSString*) image;
++(Player*) initNewPlayer1:(GameScene*) whichScene1 startingPoint: (CGPoint) location;
++(Player*) initNewPlayer2:(GameSceneLevel2*) whichScene2 startingPoint:(CGPoint)location;
 
-+(Player*) initNewPlayer:(GameScene*) whichScene startingPoint: (CGPoint) location;
+typedef enum: int{
+    PlayerFacingLeft ,
+    PlayerFacingRight,
+    PlayerRunningLeft,
+    PlayerRunningRight,
+    PlayerSkiddingLeft,
+    PlayerSkiddingRight,
+    PlayerJumpingLeft,
+    PlayerJumpingRight,
+    PlayerJumpingUpFacingLeft,
+    PlayerJumpingUpFacingRight
+}PlayerStatus;
 
+
+@property PlayerStatus playerStatus;
+
+-(void) runOnPlace;
 -(void) runRight;
--(void) jumpRight;
+-(void) runLeft;
+
+-(void) jump;
+-(void) skidRight;
+-(void) skidLeft;
 @end
