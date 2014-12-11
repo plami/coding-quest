d813bd997af4e58b4079792aaa58d43689126d96
emiliev1994@gmail.com <emiliev1994@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-12-11 13:06:06 +0000

    mislq che go opsravihme nai setne

--- trunk/CodingQuest/Player.m
+++ trunk/CodingQuest/Player.m
@@ -3,35 +3,177 @@
 @interface Player ()
 
 @property SKTextureAtlas* runAtlas;
-@property NSString* heroImage;
-
 @property SpriteTextures* spriteTextures;
+
+
 @end
 
 @implementation Player
 
+//putting the player in the Scene for Level 1
 
-+(Player *)initNewPlayer:(GameScene *)whichScene startingPoint:(CGPoint)location{
++(Player *)initNewPlayer1:(GameScene *)whichScene1 startingPoint:(CGPoint)location{
+    
+    SpriteTextures* playerTexture = [[SpriteTextures alloc]init];
+    [playerTexture createRunningAnimation];
+    [playerTexture createJumpingAnimation];
+    [playerTexture createSkiddingAnimation];
+    [playerTexture createStillAnimation];
+    SKTexture* f1 = [SKTexture textureWithImageNamed:@"playerStillRight.png"];
+    
+    Player *player1 = [Player spriteNodeWithTexture:f1];
+    player1.position = location;
+    player1.spriteTextures = playerTexture;
+    player1.size = CGSizeMake(50, 70);
+    player1.physicsBody.affectedByGravity = YES;
+    [whichScene1 addChild:player1];
+    
+    return player1;
+}
+
+//putting the player in the Scene for Level 2
+
++(Player *)initNewPlayer2:(GameSceneLevel2 *)whichScene2 startingPoint:(CGPoint)location{
     
     SpriteTextures* playerTexture = [[SpriteTextures alloc]init];
     [playerTexture createRunningAnimation];
     [playerTexture createJumpingAnimation];
     
-    SKTexture* f1 = [SKTexture textureWithImageNamed:@"player1.png"];
+    SKTexture* f2 = [SKTexture textureWithImageNamed:@"playerStillRight.png"];
     
-    Player *player = [Player spriteNodeWithTexture:f1];
-    player.position = location;
-    player.spriteTextures = playerTexture;
-    player.physicsBody.affectedByGravity = YES;
-    [whichScene addChild:player];
-    return player;
+    Player *player2 = [Player spriteNodeWithTexture:f2];
+    player2.position = location;
+    player2.spriteTextures = playerTexture;
+    player2.size = CGSizeMake(50, 70);
+    player2.physicsBody.affectedByGravity = YES;
+    [whichScene2 addChild:player2];
+    
+    return player2;
+}
+
+
+-(void)runOnPlace{
+    SKAction* runAction = [SKAction animateWithTextures:[self.spriteTextures runningRight] timePerFrame:0.1];
+    
+    SKAction* runForever = [SKAction repeatActionForever:runAction];
+    [self runAction:runForever];
+}
+
+-(void)runRight{
+
+   [self runOnPlace];
+    
+    SKAction* moveRight = [SKAction moveByX:50 y:0 duration:1];
+    SKAction* moveForever = [SKAction repeatActionForever:moveRight];
+    
+    [self runAction:moveForever];
+    _playerStatus = PlayerRunningRight;
     
 }
 
--(void)jumpRight{
+-(void)skidRight{
     
-    SKAction* jumpAction = [SKAction animateWithTextures:[_spriteTextures jumpingRight] timePerFrame:0.3];
-    [self runAction:jumpAction];
+    [self removeAllActions];
+    _playerStatus = PlayerSkiddingRight ;
+    
+    SKAction* skidAnimation = [SKAction animateWithTextures:[_spriteTextures skiddingRight] timePerFrame:1];
+    SKAction* skidAwhile = [SKAction repeatAction:skidAnimation count:0.2];
+    
+    SKAction* moveLeft = [SKAction moveByX:-50 y:0 duration:1];
+    SKAction* moveAwile = [SKAction repeatAction:moveLeft count:1];
+    
+    SKAction* stillAnimation = [SKAction animateWithTextures:[_spriteTextures stillRight] timePerFrame:1];
+    
+    SKAction* stillAwhile = [SKAction repeatAction:stillAnimation count:0.1];
+    
+    SKAction* sequence = [SKAction sequence:@[skidAwhile, moveAwile, stillAwhile]];
+
+    SKAction* group = [SKAction group:@[sequence]];
+   
+    [self runAction:group completion:^{
+        
+        _playerStatus = PlayerFacingRight;
+    }];
+    
+}
+
+-(void)skidLeft{
+
+    [self removeAllActions];
+    _playerStatus = PlayerSkiddingRight ;
+    
+    SKAction* skidAnimation = [SKAction animateWithTextures:[_spriteTextures skiddingLeft] timePerFrame:1];
+    SKAction* skidAwhile = [SKAction repeatAction:skidAnimation count:0.2];
+    
+    SKAction* moveRight = [SKAction moveByX:50 y:0 duration:1];
+    SKAction* moveAwile = [SKAction repeatAction:moveRight count:1];
+    
+    SKAction* stillAnimation = [SKAction animateWithTextures:[_spriteTextures stillLeft] timePerFrame:1];
+    
+    SKAction* stillAwhile = [SKAction repeatAction:stillAnimation count:0.1];
+    
+    SKAction* sequence = [SKAction sequence:@[skidAwhile, moveAwile, stillAwhile]];
+    
+    SKAction* group = [SKAction group:@[sequence]];
+    
+    [self runAction:group completion:^{
+        
+        _playerStatus = PlayerFacingLeft;
+    }];
+
+    
+}
+
+-(void) runLeft{
+    
+    SKAction* runAction = [SKAction animateWithTextures:[_spriteTextures runningLeft] timePerFrame:0.1f];
+    
+    SKAction* runForever = [SKAction repeatActionForever:runAction];
+    [self runAction:runForever];
+    
+    SKAction* moveLeft = [SKAction moveByX:-80 y:0 duration:1];
+    SKAction* moveForever = [SKAction repeatActionForever:moveLeft];
+    
+    [self runAction:moveForever];
+    _playerStatus = PlayerRunningLeft;
+
+}
+
+
+-(void)jump{
+    
+    PlayerStatus nextPlayerStatus = 0;
+    NSArray* playerJumpTexture = nil;
+    
+    if(self.playerStatus == PlayerRunningLeft || self.playerStatus == PlayerSkiddingLeft){
+        
+        self.playerStatus = PlayerJumpingLeft;
+        playerJumpTexture = _spriteTextures.jumpingLeft;
+        nextPlayerStatus = PlayerRunningLeft;
+    }
+    else if(self.playerStatus == PlayerRunningRight || self.playerStatus == PlayerSkiddingRight){
+        
+        self.playerStatus = PlayerJumpingRight;
+        playerJumpTexture = _spriteTextures.jumpingRight;
+        nextPlayerStatus = PlayerRunningRight;
+    }
+    else if(self.playerStatus == PlayerFacingLeft){
+        self.playerStatus = PlayerJumpingUpFacingLeft;
+        playerJumpTexture = _spriteTextures.jumpingLeft;
+        nextPlayerStatus = PlayerFacingRight;
+    }
+    else if(self.playerStatus == PlayerFacingRight){
+        self.playerStatus = PlayerJumpingUpFacingRight;
+        playerJumpTexture = _spriteTextures.jumpingRight;
+        nextPlayerStatus = PlayerFacingRight;
+    }
+    else{
+        NSLog(@"invalid value :/");
+    }
+    
+    
+    SKAction* jumpAnimation = [SKAction animateWithTextures:playerJumpTexture timePerFrame:0.2];
+    [self runAction:jumpAnimation];
     
     SKAction *moveUp = [SKAction moveBy:CGVectorMake(0, 100) duration:0.8];
     
@@ -39,32 +181,45 @@
     SKAction *seq = [SKAction sequence:@[moveUp, moveDown]];
     
     [self runAction:seq];
+    SKAction* jumpAwhile = [SKAction repeatAction:jumpAnimation count:1];
+    SKAction* groupedJump = [SKAction group:@[jumpAwhile]];
+    
+
+    [self runAction:groupedJump completion:^{
+        
+        if(nextPlayerStatus == PlayerRunningLeft){
+            [self removeAllActions];
+            [self runLeft];
+
+        }
+        else if(nextPlayerStatus == PlayerRunningRight){
+            [self removeAllActions];
+            [self runRight];
+        }
+        else if(nextPlayerStatus == PlayerFacingLeft){
+            NSArray* playerStillTextures = _spriteTextures.stillLeft;
+            
+            SKAction* stillAnimation = [SKAction animateWithTextures:playerStillTextures timePerFrame:1];
+            SKAction* stilAwile = [SKAction repeatAction:stillAnimation count:0.1];
+            
+            [self runAction:stilAwile];
+            self.playerStatus = PlayerFacingLeft;
+        }
+        else if(nextPlayerStatus == PlayerFacingRight){
+            NSArray* playerStillTextures = _spriteTextures.stillRight;
+            
+            SKAction* stillAnimation = [SKAction animateWithTextures:playerStillTextures timePerFrame:1];
+            SKAction* stilAwile = [SKAction repeatAction:stillAnimation count:0.1];
+            
+            [self runAction:stilAwile];
+            self.playerStatus = PlayerFacingRight;
+        }
+        else{
+            NSLog(@"something went wrong :<");
+        }
+    }];
     
 }
-
--(void)runRight{
-    
-    SKAction* runAction = [SKAction animateWithTextures:[self.spriteTextures runningRight] timePerFrame:0.1];
-    
-    SKAction* runForever = [SKAction repeatActionForever:runAction];
-    [self runAction:runForever];
-  
-    SKAction* moveRight = [SKAction moveByX:100 y:0 duration:1];
-    SKAction* moveForever = [SKAction repeatActionForever:moveRight];
-    
-    [self runAction:moveForever];
-    
-}
-
--(void) runLeft{
-    SKAction* runAction = [SKAction animateWithTextures:[_spriteTextures runningLeft] timePerFrame:0.1f];
-    
-    SKAction* runForever = [SKAction repeatActionForever:runAction];
-    [self runAction:runForever];
-    
-    SKAction* moveLeft = [SKAction moveByX:100 y:0 duration:1];
-}
-
 
 
 @end
