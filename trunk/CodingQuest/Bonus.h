d936f699ac45042c9637fad383673cb1374a6064
plamena.y.nikolova@gmail.com <plamena.y.nikolova@gmail.com@d63ec17d-1ecc-fb2d-0967-87e9034c78c1>
2014-12-12 15:34:22 +0000

    Bonuses added

---
+++ trunk/CodingQuest/Bonus.h
@@ -0,0 +1,18 @@
+#import <SpriteKit/SpriteKit.h>
+#import "SpriteTextures.h"
+#import "GameScene.h"
+
+@interface Bonus : SKSpriteNode
+
+typedef enum: int{
+    CoinFacingLeft
+}BonusStatus;
+
+@property BonusStatus bonusStatus;
+
++(Bonus*) initNewBonus: (SKScene*)whichScene startingPoint:(CGPoint) location;
+
+-(void)addedInScene:(SKScene *)whichScene;
+-(void) moveLeft;
+
+@end
